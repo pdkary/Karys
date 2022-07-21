72419e608c898c8c8c5bb9672a9d86d60df33b2a
pdkary <pdkary@gmail.com>
2022-07-21 19:28:40 -0400

    restructured class trainer

--- trainers/ClassificationTrainer.py
+++ trainers/ClassificationTrainer.py
@@ -39,21 +39,6 @@ class ClassificationTrainer(object):
                 classifier_grads = grad_tape.gradient(loss, self.classifier.model.trainable_variables)
                 self.classifier.optimizer.apply_gradients(zip(classifier_grads, self.classifier.model.trainable_variables))
         return np.sum(loss_bucket, axis=0)
-
-    def __run_batch__(self, batch, labels, training=True):
-        _, class_probs, predicted_labels = self.classifier.classify(batch,training)
-        self.most_recent_target_output = list(zip(class_probs, predicted_labels))
-        return self.classifier.loss(labels, class_probs)
-    
-    def train_target_batch(self, target_batch, training=True):
-        batch_size = target_batch.shape[0]
-        target_label = self.classifier.label_generator.get_single_category(0, batch_size)
-        return self.__run_batch__(target_batch, target_label, training)
-    
-    def train_noise_batch(self, noise_batch, training=True):
-        batch_size = noise_batch.shape[0]
-        noise_label = self.classifier.label_generator.get_single_category(2, batch_size)
-        return self.__run_batch__(noise_batch, noise_label, training)
     
     def __test__(self, dataset, batch_function, batch_size, num_batches, training=True):
         loss_bucket = []
@@ -65,6 +50,20 @@ class ClassificationTrainer(object):
             loss = batch_function(i,training)
             loss_bucket.append(loss)
         return np.sum(loss_bucket, axis=0)
+    
+    def train_target_batch(self, target_batch, training=True):
+        batch_size = target_batch.shape[0]
+        target_label = self.classifier.label_generator.get_single_category(0, batch_size)
+        _, class_probs, predicted_labels = self.classifier.classify(target_batch, training)
+        self.most_recent_target_output = list(zip(target_batch, predicted_labels))
+        return self.classifier.loss(target_label, class_probs)
+    
+    def train_noise_batch(self, noise_batch, training=True):
+        batch_size = noise_batch.shape[0]
+        noise_label = self.classifier.label_generator.get_single_category(2, batch_size)
+        _, class_probs, predicted_labels = self.classifier.classify(noise_batch, training)
+        self.most_recent_noise_output = list(zip(noise_batch, predicted_labels))
+        return self.classifier.loss(noise_label, class_probs)
 
     def train_target(self, batch_size, num_batches):
         return self.__train__(self.target_train_data, self.train_target_batch, batch_size, num_batches)
