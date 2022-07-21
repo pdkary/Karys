fc82bf0b63f4fdc734431f7976659acfa9d8c35c
pdkary <pdkary@gmail.com>
2022-07-21 17:54:30 -0400

    separated noise and target training

--- trainers/ClassificationTrainer.py
+++ trainers/ClassificationTrainer.py
@@ -29,63 +29,55 @@ class ClassificationTrainer(object):
         self.label_generator = BatchedCategoricalLabel(3, label_dict)
         self.most_recent_target_output = None
         self.most_recent_noise_output = None
-
-    def test_step(self, target_input, noise_input, training=True):
-        batch_size = target_input.shape[0]
-        classified_target = self.classifier.model(target_input, training=training)
-        classified_noise = self.classifier.model(noise_input, training=training)
-
+    
+    def train_target_batch(self, target_batch, training=True):
+        batch_size = target_batch.shape[0]
+        classified_target = self.classifier.model(target_batch, training=training)
         argmax_target = np.argmax(classified_target, axis=1)
-        argmax_noise = np.argmax(classified_noise, axis=1)
+
         target_class_labels = [self.label_generator.label_dict[x] for x in argmax_target]
+        self.most_recent_target_output = list(zip(target_batch, target_class_labels))
+        
+        target_label = self.label_generator.get_single_categories(0, batch_size)
+        target_loss = self.classifier.loss(target_label, classified_target)
+        return target_loss
+    
+    def train_noise_batch(self, noise_batch, training=True):
+        batch_size = noise_batch.shape[0]
+        classified_noise = self.classifier.model(noise_batch, training=training)
+        argmax_noise = np.argmax(classified_noise, axis=1)
+
         noise_class_labels = [self.label_generator.label_dict[x] for x in argmax_noise]
+        self.most_recent_noise_output = list(zip(noise_batch, noise_class_labels))
+        
+        noise_label = self.label_generator.get_single_categories(2, batch_size)
+        noise_loss = self.classifier.loss(noise_label, classified_noise)
+        return noise_loss
 
-        self.most_recent_target_output = list(zip(target_input, target_class_labels))
-        self.most_recent_noise_output = list(zip(noise_input, noise_class_labels))
+    def __train__(self, dataset, batch_function, batch_size, num_batches, training=True):
+        loss_bucket = []
+        batched_input = batch(dataset, batch_size)
+        np.random.shuffle(batched_input)
 
-        hot_dog_label = self.label_generator.get_single_categories(0, batch_size)
-        not_hot_dog_label = self.label_generator.get_single_categories(2, batch_size)
-
-        target_loss = self.classifier.loss(hot_dog_label, classified_target)
-        noise_loss  = self.classifier.loss(not_hot_dog_label, classified_noise)
-        return target_loss, noise_loss
-
-    def train(self, batch_size, num_batches) -> np.float32:
-        target_loss_bucket, noise_loss_bucket = [], []
-
-        batched_target_input = batch(self.target_train_data, batch_size)
-        batched_noise_input = batch(self.noise_train_data, batch_size)
-
-        np.random.shuffle(batched_target_input)
-        np.random.shuffle(batched_noise_input)
-
-        target_input_dataset = batched_target_input[:num_batches]
-        noise_input_dataset = batched_noise_input[:num_batches]
-
-        for target_input, noise_input in zip(target_input_dataset, noise_input_dataset):
+        input_dataset = batched_input[:num_batches]
+        for i in input_dataset:
             with tf.GradientTape() as grad_tape:
-                target_loss, noise_loss = self.test_step(target_input, noise_input)
-                target_loss_bucket.append(target_loss)
-                noise_loss_bucket.append(noise_loss)
+                loss = batch_function(i,training)
+                loss_bucket.append(loss)
                 
-                classifier_grads = grad_tape.gradient(target_loss + noise_loss, self.classifier.model.trainable_variables)
+                classifier_grads = grad_tape.gradient(loss, self.classifier.model.trainable_variables)
                 self.classifier.optimizer.apply_gradients(zip(classifier_grads, self.classifier.model.trainable_variables))
-        return np.sum(target_loss_bucket,axis=0), np.sum(noise_loss_bucket,axis=0)
+        return np.sum(loss_bucket, axis=0)
 
-    def test(self, batch_size, num_batches) -> np.float32:
-        target_loss_bucket, noise_loss_bucket = [], []
+    def train_target(self, batch_size, num_batches):
+        return self.__train__(self.target_train_data, self.train_target_batch, batch_size, num_batches)
 
-        batched_target_input = batch(self.target_test_data, batch_size)
-        batched_noise_input = batch(self.noise_test_data, batch_size)
+    def test_target(self, batch_size, num_batches):
+        return self.__train__(self.target_test_data, self.train_target_batch, batch_size, num_batches, training=False)
 
-        np.random.shuffle(batched_target_input)
-        np.random.shuffle(batched_noise_input)
-        target_input_dataset = batched_target_input[:num_batches]
-        noise_input_dataset = batched_noise_input[:num_batches]
+    def train_noise(self, batch_size, num_batches):
+        return self.__train__(self.noise_train_data, self.train_noise_batch, batch_size, num_batches)
 
-        for target_input, noise_input in zip(target_input_dataset, noise_input_dataset):
-            target_loss, noise_loss = self.test_step(target_input, noise_input, training=False)
-            target_loss_bucket.append(target_loss)
-            noise_loss_bucket.append(noise_loss)
-        return np.sum(target_loss_bucket,axis=0), np.sum(noise_loss_bucket,axis=0)
+    def test_target(self, batch_size, num_batches):
+        return self.__train__(self.noise_test_data, self.train_noise_batch, batch_size, num_batches, training=False)
         
