33367304d8c36007fca9065756efcf8c3dd434ae
pdkary <pdkary@gmail.com>
2022-07-31 14:02:10 -0400

    more exp

--- trainers/EncoderTrainer.py
+++ trainers/EncoderTrainer.py
@@ -47,10 +47,7 @@ class EncoderTrainer(object):
         e_classifier_loss = self.encoded_classifier.classifier.loss(labels, e_probs)
         i_classifier_loss = self.image_classifier.loss(labels, c_probs)
 
-        encoder_real_std = np.std(encoded_batch, axis=0)
-        encoder_real_std_loss = self.encoded_classifier.encoder.loss(0.3*np.ones_like(encoder_real_std), encoder_real_std)
-
-        encoder_loss = 0.25*encoder_real_std_loss + e_classifier_loss
+        encoder_loss = self.encoded_classifier.encoder.loss(c_probs, e_probs)
         return encoder_loss, e_classifier_loss, i_classifier_loss
 
     def train(self, batch_size, num_batches):
