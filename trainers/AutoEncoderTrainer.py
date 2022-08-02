441329fcd424d04f3ce78d91df7660eb535c28c2
pdkary <pdkary@gmail.com>
2022-08-02 19:45:22 -0400

    simplifying loss in auto enc

--- trainers/AutoEncoderTrainer.py
+++ trainers/AutoEncoderTrainer.py
@@ -38,23 +38,16 @@ class AutoEncoderTrainer(object):
         self.most_recent_gen_encoding = None
         self.most_recent_real_classification = None
         self.most_recent_gen_classification = None
-    #experimental direct loss
-    def decoder_loss(self, input_images, decoded_images):
-        return self.auto_encoder.decoder.loss(input_images, decoded_images)
-        
-    # def decoder_loss(self, label_vectors, encoded_label_probs, reencoded_label_probs, encoded_vectors, reencoded_vectors):
-        # encoding_loss = self.encoding_loss_coefficient*self.auto_encoder.decoder.loss(encoded_vectors, reencoded_vectors)
-        # label_loss = self.auto_encoder.decoder.loss(label_vectors, reencoded_label_probs)
-        # reencoding_loss = self.re_encoding_diff_coefficient*self.auto_encoder.decoder.loss(encoded_label_probs, reencoded_label_probs)
-        # return encoding_loss + label_loss + reencoding_loss
+
+    ##generator should create images that are correctly identified and DO NOT receive the reencoding flag
+    def decoder_loss(self, label_vectors, reencoded_label_probs):
+        return self.auto_encoder.decoder.loss(label_vectors, reencoded_label_probs)
     
-    def encoder_loss(self,label_vectors, encoded_label_probs, reencoded_label_probs, encoded_vectors, reencoded_vectors):
-        lbl_loss = self.auto_encoder.encoder.loss(label_vectors, encoded_label_probs)
-        reenc_lbl_loss = self.auto_encoder.encoder.loss(label_vectors, reencoded_label_probs)
-        reenc_diff_lbl_loss = self.re_encoding_diff_coefficient*self.auto_encoder.encoder.loss(encoded_label_probs, reencoded_label_probs)
-        vector_diff_loss = self.encoding_loss_coefficient*self.auto_encoder.encoder.loss(encoded_vectors, reencoded_vectors)
-        return lbl_loss + reenc_lbl_loss + reenc_diff_lbl_loss + vector_diff_loss
+    ##encoder should create vectors that are correctly identified and DO NOT receive the reencoding flag
+    def encoder_loss(self,label_vectors, encoded_label_probs):
+        return self.auto_encoder.encoder.loss(label_vectors, encoded_label_probs)
     
+    ##classifier should correctly identify reals without reencoding flag, and reencodings with the reencoding flag
     def classifier_loss(self, label_vectors, encoded_label_probs, reencoded_label_probs):
         batch_size = label_vectors.shape[0]
         re_encoded_flag_vector = self.auto_encoder.classifier.label_generator.get_label_vector_by_category_name("generated")
@@ -71,8 +64,8 @@ class AutoEncoderTrainer(object):
 
         V, V_probs, V_lbls, gen_batch, GV, GV_probs, GV_lbls = self.auto_encoder.run_all(batch_data, training)
 
-        decoder_loss = self.decoder_loss(batch_data,gen_batch)
-        encoder_loss = self.encoder_loss(labels, V_probs, GV_probs, V, GV)
+        decoder_loss = self.decoder_loss(labels, GV_probs)
+        encoder_loss = self.encoder_loss(labels, V_probs)
         classifier_loss = self.classifier_loss(labels, V_probs, GV_probs)
 
         self.most_recent_generated = gen_batch
