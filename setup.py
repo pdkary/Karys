2d58f7bc91edb9c0cec0d6951b4b5e5d3f5becd1
pdkary <pdkary@gmail.com>
2022-11-03 20:37:15 -0400

    exp with setup again

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ setup(name='Karys',
       description='Personal implementations of popular ML structures',
       author='Parker Kary',
       author_email='pdkary@gmail.com',
-      packages=find_namespace_packages(include=['karys.*']),
+      packages=[
+          'karys', 'karys.data', 'karys.image_utils', 'karys.layers',
+          'karys.models', 'karys.plotting', 'karys.testing', 'karys.trainers'
+      ],
       install_requires=[
-        'pandas',
-        'tensorflow',
-        'tensorflow_addons',
-        'tensorflow-gpu==2.9.1',
-        'numpy',
-        'jupyter',
-        'jupyterplot',
-        'legitindicators',
-        'scikit-learn',
-        'dictdiffer',
-    ]
-)
+          'pandas',
+          'tensorflow',
+          'tensorflow_addons',
+          'tensorflow-gpu==2.9.1',
+          'numpy',
+          'jupyter',
+          'jupyterplot',
+          'legitindicators',
+          'scikit-learn',
+          'dictdiffer',
+      ])
