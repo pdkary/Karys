379420382077730e9d7f10bf0f9ebf36accc20f5
pdkary <parker@pgcwi.com>
2024-03-16 16:05:43 -0400

    test

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
-from examples.progressive_gan import progressive_gan 
+# from examples.progressive_gan import progressive_gan 
 # from examples.progressive_discriminator import progressive_discriminator 
+from examples.text_rnn import text_rnn
+text_rnn.train(50,2,5000)
 
-
-progressive_gan.train(2000, 100)
+# progressive_gan.train(2000, 100)
 # progressive_discriminator.train(2000, 10)
