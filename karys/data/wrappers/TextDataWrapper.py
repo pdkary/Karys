142acc1896766ebd15ba4d4bd0ed336facf18e05
Kary-pgcw <parker@pgcwi.com>
2024-03-16 16:00:51 -0400

    added basis for text rnn

--- karys/data/wrappers/TextDataWrapper.py
+++ karys/data/wrappers/TextDataWrapper.py
@@ -1,71 +1,52 @@
-from typing import Dict
-import numpy as np
-from keras.preprocessing.text import Tokenizer
+import re
+import string
 import tensorflow as tf
-from karys.data.configs.TextDataConfig import TextDataConfig
-from karys.data.wrappers.DataWrapper import DataWrapper
 
-class TextDataWrapper(DataWrapper):
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  return tf.strings.regex_replace(lowercase,
+                                  '[%s]' % re.escape(string.punctuation), '')
+class TextDataWrapper():
     def __init__(self, 
-                 word_index, 
-                 input_sentences,
-                 label_sentences,
-                 data_config: TextDataConfig, 
-                 train_test_ratio: float = 0.7,):
-        super(TextDataWrapper, self).__init__(data_config, train_test_ratio)
-        self.word_index: Dict = word_index
-        self.input_sentences = input_sentences
-        self.label_sentences = label_sentences
-        self.index_to_word = {v:k for k, v in word_index.items()}
+                 input_text_path: str,
+                 input_length: int,
+                 output_length: int,
+                 train_test_ratio: float = 0.7):
+        self.validation_percentage = 1 - train_test_ratio
+        self.input_path = input_text_path
+        with open(self.input_path, 'r') as f:
+            self.input_text = f.read()
+        
+        self.words = self.input_text.split()
+        self.vocab = sorted(set(self.words))
+        self.vocab_size = len(self.vocab)
+        self.word_to_index = {word: index for index, word in enumerate(self.vocab)}
+        self.index_to_word = {index: word for index, word in enumerate(self.vocab)}
+        
+        self.input_sequences = []
+        self.output_sequences = []
+        for i in range(len(self.words) - input_length - output_length):
+            input_seq = self.words[i:i + input_length]
+            output_seq = self.words[i + input_length:i + input_length + output_length]
+            self.input_sequences.append([self.word_to_index[word] for word in input_seq])
+            self.output_sequences.append([self.word_to_index[word] for word in output_seq])
 
     @property
     def size(self):
-        return len(self.input_sentences)
-    
-    @property
-    def word_size(self):
-        return len(self.word_index)
-    
-    def scale_batch(self,batch):
-        return batch/self.word_size
-    
-    def unscale_batch(self,batch):
-        return (self.word_size*batch).astype(int)
-    
-    @classmethod
-    def load_from_file(cls, filename: str, data_config: TextDataConfig):
-        with open(filename, 'r') as f:
-            corpus = f.readlines()
-        tokenizer = Tokenizer(num_words=data_config.vocab_size)
-        tokenizer.fit_on_texts(corpus)
-
-        word_index = tokenizer.word_index
-        train_sequences = tokenizer.texts_to_sequences(corpus)
-
-        input_sets = []
-        label_sets = []
-        IL= data_config.input_length
-        OL = data_config.output_length
-        L = IL+OL
-        for sequence in train_sequences:
-            N = len(sequence)
-            for i in range(N-L):
-                input_sets.append(sequence[i:i+IL])
-                label_sets.append(sequence[i+IL:i+L])
-        return cls(word_index, np.array(input_sets), np.array(label_sets), data_config)
+        return len(self.input_sequences)
     
     def get_train_dataset(self):
         validation_size = int(self.validation_percentage*self.size)
-        sentence_inputs = self.input_sentences[:-validation_size]
-        sentence_labels = self.label_sentences[:-validation_size]
-        train_data = tf.data.Dataset.from_tensor_slices((sentence_inputs,sentence_labels))
+        sentence_inputs = self.input_sequences[:-validation_size]
+        sentence_outputs = self.output_sequences[:-validation_size]
+        train_data = list(zip(sentence_inputs,sentence_outputs))
         return train_data
     
     def get_validation_dataset(self):
         validation_size = int(self.validation_percentage*self.size)
-        sentence_inputs = self.input_sentences[-validation_size:]
-        sentence_labels = self.label_sentences[-validation_size:]
-        test_data = tf.data.Dataset.from_tensor_slices((sentence_inputs,sentence_labels))
+        sentence_inputs = self.input_sequences[-validation_size:]
+        sentence_outputs = self.output_sequences[-validation_size:]
+        test_data = list(zip(sentence_inputs, sentence_outputs))
         return test_data
 
     def translate_sentence(self, sentence):
@@ -75,7 +56,7 @@ class TextDataWrapper(DataWrapper):
         return [self.translate_sentence(sentence) for sentence in sentence_list]
 
     def show_sentence_n(self,n):
-        sin = self.input_sentences[n]
-        son = self.label_sentences[n]
+        sin = self.input_sequences[n]
+        son = self.output_sequences[n]
         print("sentence in:\n", self.translate_sentence(sin), sin.shape)
         print("\nsentence out:\n", self.translate_sentence(son), son.shape)
